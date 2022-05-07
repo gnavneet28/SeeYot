917178362885b59acec8731d55ed05248de13e96
gnavneet28 <sggauravn@gmail.com>
2022-05-07 23:02:03 +0530

    Modified ChatBackgroundImage

--- app/components/ChatBackgroundSelector.js
+++ app/components/ChatBackgroundSelector.js
@@ -1,45 +1,17 @@
 import React from "react";
 import { StyleSheet, ImageBackground } from "react-native";
-import LinearGradient from "react-native-linear-gradient";
 
 import defaultStyles from "../config/styles";
 
 function ChatBackgroundSelector({ children, activeChat }) {
-  if (!activeChat)
-    return (
-      <ImageBackground
-        resizeMode="cover"
-        source={{ uri: "chat_wallpaper" }}
-        style={{
-          height: "100%",
-          width: "100%",
-          alignItems: "center",
-          backgroundColor: defaultStyles.colors.white,
-        }}
-      >
-        {children}
-      </ImageBackground>
-    );
   return (
-    <LinearGradient
-      colors={
-        !activeChat
-          ? [
-              defaultStyles.colors.white,
-              defaultStyles.colors.white,
-              defaultStyles.colors.white,
-            ]
-          : [
-              defaultStyles.colors.lightGrey,
-              defaultStyles.colors.lightGrey,
-              defaultStyles.colors.primary,
-            ]
-      }
-      style={styles.container}
-      locations={[0.0, 0.2, 1.0]}
+    <ImageBackground
+      resizeMode="cover"
+      source={{ uri: "chatwallpaper" }}
+      style={styles.imageBackgroundContainer}
     >
       {children}
-    </LinearGradient>
+    </ImageBackground>
   );
 }
 
@@ -48,6 +20,12 @@ const styles = StyleSheet.create({
     width: "100%",
     flex: 1,
     alignItems: "center",
+  },
+  imageBackgroundContainer: {
+    alignItems: "center",
+    backgroundColor: defaultStyles.colors.white,
+    height: "100%",
+    width: "100%",
   },
 });
 
